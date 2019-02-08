8d2cc14f82482121c1366d4ba79e86ae6a38c267
Sven Röttering <sven.roettering@10m.de>
2019-02-08 11:39:16 +0100

    Use browserify instead of babel

--- src/modes/stamp-mode.js
+++ src/modes/stamp-mode.js
@@ -1,4 +1,45 @@
-import { stamp } from '../util/stamp';
+const difference = require('@turf/difference');
+const flatten = require('@turf/flatten').default;
+const intersect = require('@turf/intersect').default;
+const featureCollection = require('@turf/helpers').featureCollection;
+const lineString = require('@turf/helpers').lineString;
+const rewind = require('@turf/rewind').default;
+const truncate = require('@turf/truncate').default;
+
+/**
+ * Takes a polygon base and a polygon mask and slices the base according to the mask
+ *
+ * @param originalPolygon The polygon to slice
+ * @param mask The polygon to slice with
+ * @returns {*} A FeatureCollection with the resulting Polygons. May be empty
+ */
+const stamp = (originalPolygon, mask) => {
+    const innerPolygon = intersect(originalPolygon, mask);
+    if (!innerPolygon) {
+        return featureCollection([originalPolygon]);
+    }
+
+    // Difference between polygons by clipping the second polygon from the first
+    const outerPolygons = difference(originalPolygon, mask);
+    if (!outerPolygons) {
+        // Polygon and mask are identical
+        return featureCollection([innerPolygon]);
+    }
+
+    // We have to truncate the inner polygon rings of the outer polygon, because intersect truncates as well
+    const flattenedOuterPolygons = flatten(outerPolygons).features;
+    flattenedOuterPolygons.forEach(outerPolygon => {
+        outerPolygon.geometry.coordinates.map((coords, index) => {
+            if (index !== 0) {
+                return truncate(lineString(coords), { precision: 6, mutate: true }).geometry.coordinates;
+            }
+            return coords;
+        });
+    });
+
+    const slicedPolygons = featureCollection([...flatten(innerPolygon).features, ...flattenedOuterPolygons]);
+    return rewind(slicedPolygons);
+};
 
 const doubleClickZoom = {
     enable(ctx) {
@@ -25,20 +66,25 @@ const doubleClickZoom = {
     }
 };
 
-const StampMode = MapboxDraw.modes['draw_polygon'];
+const StampMode = { ...MapboxDraw.modes['draw_polygon'] };
 
 const originalSetup = MapboxDraw.modes['draw_polygon'].onSetup;
 
-StampMode.onSetup = function(options) {
+StampMode.onSetup = function (options) {
     if (!options.baseFeatureCollection || !options.baseFeatureCollection.features.length) {
-        throw new Error('Stamping mode requires something to stamp on');
+        throw new Error('Stamping mode requires a FeatureCollection to stamp on');
     }
+
+    if (options.baseFeatureCollection.features.some((feature) => feature.type !== 'Polygon')) {
+        throw new Error('Stamping mode is only supported for Polygons');
+    }
+
     this.baseFeatureCollection = options.baseFeatureCollection;
 
     return originalSetup.apply(this, options);
 };
 
-StampMode.onStop = function(state) {
+StampMode.onStop = function (state) {
     this.updateUIClasses({ mouse: 'none' });
     doubleClickZoom.enable(this);
     this.activateUIButton();
@@ -49,7 +95,7 @@ StampMode.onStop = function(state) {
     }
 
     // remove last added coordinate
-    state.polygon.removeCoordinate(`0.${state.currentVertexPosition}`);
+    state.polygon.removeCoordinate(`0.${ state.currentVertexPosition }`);
 
     if (state.polygon.isValid()) {
         try {
@@ -60,14 +106,14 @@ StampMode.onStop = function(state) {
             this.map.fire('draw.stamped', {
                 features: stampedFeatures
             });
-            this.deleteFeature(state.polygon.id, { silent: true });
+            this.deleteFeature([state.polygon.id], { silent: true });
         } catch (error) {
-            this.map.fire('draw.error');
+            this.map.fire('draw.error', { error });
         }
     } else {
-        this.deleteFeature(state.polygon.id, { silent: true });
+        this.deleteFeature([state.polygon.id], { silent: true });
         this.changeMode('simple_select', {}, { silent: true });
     }
 };
 
-export default StampMode;
+module.exports = StampMode;
